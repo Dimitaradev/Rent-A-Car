2869239f3419b02391954d1c365d116d224badea
Dimitaradev <82084554+Dimitaradev@users.noreply.github.com>
2023-11-18 22:10:11 +0200

    Add files via upload

---
+++ Exercise.2.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>;
+
+void countNumberDigits(int number, int digitsCounterArray[], size_t size)
+{
+	while (number != 0)
+	{
+		int digit = number % 10;
+		digitsCounterArray[digit]++;
+
+		number /= 10;
+	}
+}
+
+bool checkIsNumberAValidPermutation(int inputArray[], int checkedArray[], size_t size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (inputArray[i] != checkedArray[i])
+			return false;
+	}
+
+	return true;
+}
+
+void generateAllPossibleNumbers(int number)
+{
+	constexpr int DIGITS_COUNTER_ARRAY_SIZE = 10;
+
+	int inputNumberDigitsCounterArray[DIGITS_COUNTER_ARRAY_SIZE] = { 0 };
+	countNumberDigits(number, inputNumberDigitsCounterArray, DIGITS_COUNTER_ARRAY_SIZE);
+
+	const int startNumber = 1000;
+	const int endNumber = 9999;
+
+	for (int i = startNumber; i <= endNumber; i++)
+	{
+		int currentNumberDigitsCounterArray[DIGITS_COUNTER_ARRAY_SIZE] = { 0 };
+		countNumberDigits(i, currentNumberDigitsCounterArray, DIGITS_COUNTER_ARRAY_SIZE);
+
+		bool isValidPermutation = checkIsNumberAValidPermutation(inputNumberDigitsCounterArray, currentNumberDigitsCounterArray, DIGITS_COUNTER_ARRAY_SIZE);
+
+		if (isValidPermutation)
+		{
+			std::cout << i << std::endl;
+		}
+	}
+}
+
+int main()
+{
+	int number;
+	std::cin >> number;
+
+	generateAllPossibleNumbers(number);
+}
